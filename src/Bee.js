c9c70fcd33c049ea86dbe7a19cfa2de7c0c43876
dhfromkorea <dhllee424@gmail.com>
2014-07-06 01:18:09 -0700

    completed

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
+// Bee should be a sub-class of Grub
 var Bee = function() {
+  // make sure the state props are registered
+  // on Bee sub-class with the right context
+	Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+// Bee.prototype.eat = function(){};
 
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,19 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  // the following props are overwriting
+  // the props of the super-classes
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.forrage = function(){
+	return 'I am too old, let me play cards instead';
+};
+RetiredForagerBee.prototype.gamble = function(treasure){
+	treasure = treasure || 'royal jelly';
+	this.treasureChest.push(treasure);
+};
